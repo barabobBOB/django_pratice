8e05be17ab4ca32a0498bab9a646012f02532551
Seyeon_Choi <choi20014830@gmail.com>
2024-03-16 03:29:02 +0900

    feat: db 테이블 추가

--- task/models.py
+++ task/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=50)
+    memo = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=30)
+    clear = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
--- user/models.py
+++ user/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
