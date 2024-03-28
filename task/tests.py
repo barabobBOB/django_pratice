8ac3edc2d7c8156915cff1990dd29011fd2d7907
Seyeon_Choi <choi20014830@gmail.com>
2024-03-28 21:02:11 +0900

    feat: test case 추가

--- task/tests.py
+++ task/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from task.models import TaskGroup
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -7,6 +7,73 @@ from django.contrib.auth import get_user_model
 
 from task.serializers import TaskGroupSerializer
 
+class TaskGroupModelTestCase(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(email='test@example.com', password='testpassword')
+
+    def test_taskgroup_creation(self):
+        group = TaskGroup.objects.create(name='group', memo='memo', user_id=self.user.id)
+        self.assertEqual(TaskGroup.objects.count(), 1)
+        self.assertEqual(group.name, 'group')
+        self.assertEqual(group.memo, 'memo')
+        self.assertEqual(group.user_id, self.user.id)
+
+class TaskGroupSerializerTestCase(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(email='test@example.com', password='testpassword')
+        self.task_group = TaskGroup.objects.create(name='group', memo='memo', user_id=self.user.id)
+
+    def test_taskgroup_serializer(self):
+        serializer = TaskGroupSerializer(instance=self.task_group)
+        expected_data = {
+            'name': 'group',
+            'memo': 'memo',
+        }
+        self.assertEqual(serializer.data, expected_data)
+
+
+class TaskGroupViewTestCase(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(email='test@example.com', password='testpassword')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.url = reverse('taskgroup-list')
+        TaskGroup.objects.create(name='group', memo='memo', user_id=self.user.id)
+
+    def test_create_taskgroup(self):
+        data = {'name': 'group', 'memo': 'memo'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TaskGroup.objects.count(), 2)
+
+    def test_get_taskgroup_list(self):
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], 'group')
+        self.assertEqual(response.data[0]['memo'], 'memo')
+
+    def test_get_taskgroup(self):
+        task_group = TaskGroup.objects.create(name='group2', memo='memo2', user_id=self.user.id)
+        url = reverse('taskgroup-detail', kwargs={'pk': task_group.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_taskgroup(self):
+        task_group = TaskGroup.objects.create(name='group2', memo='memo2', user_id=self.user.id)
+        url = reverse('taskgroup-detail', kwargs={'pk': task_group.pk})
+        data = {'name': 'group3', 'memo': 'memo3'}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_taskgroup(self):
+        task_group = TaskGroup.objects.create(name='group2', memo='memo2', user_id=self.user.id)
+        url = reverse('taskgroup-detail', kwargs={'pk': task_group.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 class TaskGroupListTestCase(TestCase):
     def setUp(self):
@@ -38,7 +105,7 @@ class TaskGroupListTestCase(TestCase):
         self.assertEqual(response.data[0]['memo'], 'memo')
 
 
-class TaskGroupDetailTestCase(TestCase):
+class TaskGroupDetailTestCase(APITestCase):
     def setUp(self):
         User = get_user_model()
         self.user = User.objects.create_user(email='test@example.com', password='testpassword')
